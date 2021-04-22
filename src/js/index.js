6a5afabab033dadf2b71b43963a74e2f791ea23f
Wtella <71620036+Wtella@users.noreply.github.com>
2021-04-22 04:39:43 -0400

    h

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,46 @@
 console.log("connected");
 
+function buildQuiz(){}
+
+function showResults(){}
+
+buildQuiz();
+submitButton.addEventListener('click', showResults);
+
 const showQuiz = document.getElementsByClassName('quiz');
 const submitButton = document.getElementsByClassName('submit');
 const showResults = document.getElementsByClassName('results');
-const myQuestions = 
+const myQuestions = [
 {
     question: "Auhhasuwnksjh asuh oiwjja?",
     answers: {
         a:"Akjask",
         b:"Bkajsh",
-        c:"Ckjashw",
+        c:"Ckjashw"
+    },
+
+    correctAnswer: "c"
+
+},
+{
+    question: "HHHHHHHHHH?",
+    answers: {
+        a:"I",
+        b: "Z",
+        c: "O"
     },
 
     correctAnswer: "a"
+},
+{
+    question: "Cats sleep ... hours out of a day.",
+    answers: {
+        a: "four",
+        b: "fifteen",
+        c: "twenty-three"
+    },
 
+    correctAnswer: "c"
 }
 
+];
