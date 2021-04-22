c1e041bc064ba5d226dc1e6cf3124a7e34213732
Wtella <71620036+Wtella@users.noreply.github.com>
2021-04-22 04:23:43 -0400

    a

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 console.log("connected");
+
+const showQuiz = document.getElementsByClassName('quiz');
+const submitButton = document.getElementsByClassName('submit');
+const showResults = document.getElementsByClassName('results');
+const myQuestions = 
+{
+    question: "Auhhasuwnksjh asuh oiwjja?",
+    answers: {
+        a:"Akjask",
+        b:"Bkajsh",
+        c:"Ckjashw",
+    },
+
+    correctAnswer: "a"
+
+}
+
