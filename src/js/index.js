fafbc5a34edf6851e82c5c952ba25810a929ce45
Wtella <71620036+Wtella@users.noreply.github.com>
2021-04-30 13:15:46 -0400

    a

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,49 @@
-console.log("connected");
+
+    if(userAnswer === currentQuestion.correctAnswer){
+        numberCorrect++;
+
+        answerContainers[questionNumber].style.color = 'green';
+    }
+
+    else{
+        answerContainers[questionNumber].style.color = 'red';
+    }
+
+
+
+buildQuiz();
+submitButton.addEventListener('click', showResults);
+
+const showQuiz = document.getElementsByClassName('quiz');
+const submitButton = document.getElementsByClassName('submit');
+const resultsContainer = document.getElementsByClassName('results');
+let myQuestions = [
+{
+    question: "Auhhasuwnksjh asuh oiwjja?",
+    answers: [
+        {option: "AA", answer:true},
+        {option:"Bkajsh",answer:false},
+        {option:"Ckjashw",answer:false}
+    ],
+},
+
+
+{
+    question: "H...?",
+    answers: [
+        {option:"Z",answer:false},
+        {option: "I",answer:true},
+        {option: "O", answer:false}
+    ],
+},
+
+{
+    question: "Cats sleep ... hours out of a day.",
+    answers: [
+        {option: "four",answer:false},
+        {option: "fifteen",answer:false},
+        {option: "twenty-three", answer:true}
+    ],
+}
+
+];
